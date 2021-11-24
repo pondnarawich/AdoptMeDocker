6f343ba9d95469e764301dd47466cee0da2c67dc
pondnarawich <pondnarawich@gmail.com>
2021-11-24 13:32:14 +0700

    -

--- Back/post/repository/dynamoDB/init.go
+++ Back/post/repository/dynamoDB/init.go
@@ -2,8 +2,6 @@ package dynamoDB
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
